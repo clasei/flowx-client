373f9d8020aa8dc20aa206efe90381543e1c1fa0
clara seijo <clasei@proton.me>
2025-02-15 17:21:18 +0100

    updated routes + moved task-list to dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskListComponent } from '../../components/task-list/task-list.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  // imports: [],
+  imports: [TaskListComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { TaskListComponent } from '../../components/task-list/task-list.component';
+// import { TaskListComponent } from '../../components/task-list/task-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TaskListComponent],
+  // imports: [TaskListComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
