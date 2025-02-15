dc157ed98630923f1119e97d9a064453b47e8e4a
clara seijo <clasei@proton.me>
2025-02-15 12:30:39 +0100

    taks-item updated to bring data from server properly

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,14 +1,35 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../../models/task.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.scss'
 })
+
+
 export class TaskItemComponent {
   @Input() task!: Task; // receives data from parent component (task-list)
+
+  getPriorityClass(priority: number): string {
+    switch (priority) {
+      case 1: return 'low';
+      case 2: return 'medium';
+      case 3: return 'high';
+      default: return 'unknown';
+    }
+  }
+
+  getPriorityText(priority: number): string {
+    switch (priority) {
+      case 1: return 'Low';
+      case 2: return 'Medium';
+      case 3: return 'High';
+      default: return 'Unknown';
+    }
+  }
 }
--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -3,4 +3,7 @@ export interface Task {
   title: string;
   description: string;
   completed: boolean;
+  priority: number;
+  createdAt: Date;
+  updatedAt?: Date;
 }
