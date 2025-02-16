5fc51c8b31b20f77fe2d8e4248977997b84793f1
clara seijo <clasei@proton.me>
2025-02-16 12:09:29 +0100

    added filter by priority: all / critical / focus / queue

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -14,7 +14,9 @@ import { TaskItemComponent } from '../task-item/task-item.component';
 })
 export class TaskListComponent implements OnInit {
   tasks: Task[] = []; // store feched tasks
+  allTasks: Task[] = []; // store all tasks to reset filtering
   isAscending: boolean = true; // sort order
+  selectedFilter: string = 'all';
 
   constructor(private taskService: TaskService) {}
   
@@ -26,7 +28,9 @@ export class TaskListComponent implements OnInit {
     this.taskService.getTasks().subscribe({
       next: (tasks) => {
         // this.tasks = tasks;
-        this.tasks = tasks.sort((a, b) => a.priority - b.priority); // sort tasks by priority
+        this.allTasks = tasks; // store all tasks to reset filtering
+        // this.tasks = tasks.sort((a, b) => a.priority - b.priority); // sort tasks by priority
+        this.tasks = [...this.allTasks].sort((a, b) => a.priority - b.priority);
         console.log('Tasks loaded:', tasks);
       },
       error: (err) => console.error('Error fetching tasks:', err)
@@ -36,6 +40,37 @@ export class TaskListComponent implements OnInit {
   toggleSortOrder(): void {
     this.isAscending = !this.isAscending;
     this.tasks.reverse();
+  }
+
+
+  getPriorityLabel(priority: number): string {
+    const labels: Record<number, string> = { 1: "critical", 2: "focus", 3: "pipeline" };
+    return labels[priority] || "queue"; // default to queue if not found
+  }
+
+  filterTasks(event: Event) {
+    const selectedFilter = (event.target as HTMLSelectElement).value;
+  
+    // full list first
+    if (selectedFilter === "all") {
+      this.tasks = [...this.allTasks];
+  
+      // reset sorting to default (priority ascending)
+      this.isAscending = true;
+      this.tasks.sort((a, b) => a.priority - b.priority);
+    } else {
+      this.tasks = this.allTasks.filter(
+        (task) => this.getPriorityLabel(task.priority) === selectedFilter
+      );
+    }
+  
+    // // make the sort order consistent
+    // if (!this.isAscending) {
+    //   this.tasks.reverse();
+    // }
+
+    // disable sorting when a filter is applied
+    this.isAscending = false;
   }
 
 }
