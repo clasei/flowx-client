67b9be0fa7cdec935b8b4cd1e87557c644725a98
clara seijo <clasei@proton.me>
2025-02-28 11:23:51 +0100

    feat: login form looking good, navbar includes logout

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
-// import { AuthWrapperService } from '../../services/auth.service';
+import { AuthWrapperService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
-  imports: [RouterModule]
+  imports: [RouterModule, CommonModule]
 })
-export class NavbarComponent { 
+// export class NavbarComponent { 
 
+//   constructor(private router: Router) {}
+
+//   menuOpen = false;
+
+//   toggleMenu() {
+//     this.menuOpen = !this.menuOpen;
+//   }
+// }
+
+export class NavbarComponent { 
+  authService = inject(AuthWrapperService);
   constructor(private router: Router) {}
 
-  menuOpen = false;
+  logout() {
+    this.authService.logout();
+  }
 
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
+  isLoggedIn(): boolean {
+    let isLoggedIn = false;
+    this.authService.isAuthenticated().subscribe(isAuth => isLoggedIn = isAuth);
+    return isLoggedIn;
   }
 }
