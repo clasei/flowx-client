1221252a9a310749a4456e0cd161016681b6d896
clara seijo <clasei@proton.me>
2025-03-04 14:25:45 +0100

    feat: login + sign up forms looking nice

--- src/app/pages/auth/signup/signup.component.ts
+++ src/app/pages/auth/signup/signup.component.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
   email = '';
