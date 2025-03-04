bca340175e462ef604d4c04fe3d9bde0c9a0dff4
clara seijo <clasei@proton.me>
2025-03-04 15:31:05 +0100

    fix: need to build home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,24 +2,12 @@ import { Routes } from '@angular/router';
 // import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { SignupComponent } from './pages/auth/signup/signup.component';
-
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { DoneTasksComponent } from './pages/done-tasks/done-tasks.component';
 import { DeletedTasksComponent } from './pages/deleted-tasks/deleted-tasks.component';
 import { AuthGuard } from './guards/auth.guard';
 
-
-// export const routes: Routes = [
-//   // { path: '', component: HomeComponent }, // landing page -- public
-//   { path: '', redirectTo: 'flowxboard', pathMatch: 'full' }, // ✅ Redirect root to dashboard
-//   { path: 'login', component: LoginComponent }, // login page
-//   { path: 'flowxboard', component: DashboardComponent }, // main page after login
-//   { path: 'settings', component: SettingsComponent }, // settings
-//   { path: 'done', component: DoneTasksComponent }, // completed Tasks
-//   { path: 'deleted', component: DeletedTasksComponent }, // deleted tasks
-//   { path: '**', redirectTo: '' } // redirect unknown routes
-// ];
 
 export const routes: Routes = [
   { path: '', redirectTo: 'flowxboard', pathMatch: 'full' }, // Redirect root to dashboard
