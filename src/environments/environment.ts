7ba61df0c7e41701fb073134da1fa768e890d065
clara seijo <clasei@proton.me>
2025-03-09 12:03:28 +0100

    style: control panel + task card ->  small screens

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'  // local
+  // apiUrl: 'http://localhost:8080'  // local
+  apiUrl: 'https://flowx-server.onrender.com/'
 };
