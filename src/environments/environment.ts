4cfda8ccb5774ceee3eb580a9539cfcc2e711bda
clara seijo <clasei@proton.me>
2025-03-09 13:08:41 +0100

    revert: environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8080'  // local
-  apiUrl: 'https://flowx-server.onrender.com/'
+  apiUrl: 'http://localhost:8080'  // local
+  // apiUrl: 'https://flowx-server.onrender.com/'
 };
